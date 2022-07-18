6409db03b253dad910b95c56dd30bcf7cfc68776
moorthy25 <58259989+moorthy25@users.noreply.github.com>
2022-07-18 11:00:17 +0530

    plus btn removed in unwanted pages & text last char focus enabled

--- src/component/Text.jsx
+++ src/component/Text.jsx
@@ -1,4 +1,4 @@
-import { useContext, useLayoutEffect } from "react";
+import { useContext, useEffect, useLayoutEffect } from "react";
 import { useRef } from "react";
 import { useTextInput } from "../hooks/TextDebounce";
 import BlockOptions from "./blockOptions";
@@ -7,9 +7,10 @@ import { todoListSetstateContext } from "./TodoContainer";
 const Text = ({ item, cb = () => { } }) => {
     const setTodo = useContext(todoListSetstateContext)
     const ele = useRef(null)
-    
-    const textfn = useTextInput(v => {
-        setTodo(p => ({ ...p, list: p.list.map(i => i.uuid === item.uuid ? { ...i, content: v.replaceAll("<br/>","\n") } : i) }))
+
+
+
+    useEffect(() => {
 
         //to focus last charcter
         const selection = window.getSelection();
@@ -18,12 +19,14 @@ const Text = ({ item, cb = () => { } }) => {
         range.selectNodeContents(ele.current);
         range.collapse(false);
         selection.addRange(range);
-        ele.current.focus();
-    })
+        
+    }, [item.content]);
+
+    const textfn = useTextInput(v => setTodo(p => ({ ...p, list: p.list.map(i => i.uuid === item.uuid ? { ...i, content: v.replaceAll("<br/>", "\n") } : i) })))
     return (
         <div className="todoContainerWraper relative mt-5">
             <BlockOptions item={item} />
-            <div placeholder="hello" dangerouslySetInnerHTML={{__html: item.content.replaceAll("\n","<br/>")}} ref={r => ele.current = r} onInput={e => textfn(e.target.innerText)} suppressContentEditableWarning contentEditable
+            <div placeholder="hello" dangerouslySetInnerHTML={{ __html: item.content.replaceAll("\n", "<br/>") }} ref={r => ele.current = r} onInput={e => textfn(e.target.innerText)} suppressContentEditableWarning contentEditable
                 className="text relative bg-[#222] border-[1px] border-solid border-white w-full min-h-[5rem] p-1 rounded-[10px] mb-2 empty:before:content-['Notes...'] before:text-[#fff8] ">
             </div>
         </div>
