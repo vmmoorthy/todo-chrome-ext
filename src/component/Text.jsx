6f7040ace396ae7629aac40f2ec13c75b0e1b83b
moorthy25 <58259989+moorthy25@users.noreply.github.com>
2022-07-18 11:28:09 +0530

    text edit currsor position stable support added

--- src/component/Text.jsx
+++ src/component/Text.jsx
@@ -1,33 +1,17 @@
-import { useContext, useEffect, useLayoutEffect } from "react";
-import { useRef } from "react";
+import { useContext, useState } from "react";
 import { useTextInput } from "../hooks/TextDebounce";
 import BlockOptions from "./blockOptions";
 import { todoListSetstateContext } from "./TodoContainer";
 
 const Text = ({ item, cb = () => { } }) => {
     const setTodo = useContext(todoListSetstateContext)
-    const ele = useRef(null)
-
-
-
-    useEffect(() => {
-
-        //to focus last charcter
-        const selection = window.getSelection();
-        const range = document.createRange();
-        selection.removeAllRanges();
-        range.selectNodeContents(ele.current);
-        range.collapse(false);
-        selection.addRange(range);
-        
-    }, [item.content]);
+    const [value] = useState(item.content);
 
     const textfn = useTextInput(v => setTodo(p => ({ ...p, list: p.list.map(i => i.uuid === item.uuid ? { ...i, content: v.replaceAll("<br/>", "\n") } : i) })))
     return (
         <div className="todoContainerWraper relative mt-5">
             <BlockOptions item={item} />
-            <div placeholder="hello" dangerouslySetInnerHTML={{ __html: item.content.replaceAll("\n", "<br/>") }} ref={r => ele.current = r} onInput={e => textfn(e.target.innerText)} suppressContentEditableWarning contentEditable
-                className="text relative bg-[#222] border-[1px] border-solid border-white w-full min-h-[5rem] p-1 rounded-[10px] mb-2 empty:before:content-['Notes...'] before:text-[#fff8] ">
+            <div placeholder="hello" dangerouslySetInnerHTML={{ __html: value.replaceAll("\n", "<br/>") }} onInput={e => textfn(e.target.innerText)} suppressContentEditableWarning contentEditable className="text relative bg-[#222] border-[1px] border-solid border-white w-full min-h-[5rem] p-1 rounded-[10px] mb-2 empty:before:content-['Notes...'] before:text-[#fff8] ">
             </div>
         </div>
     );
